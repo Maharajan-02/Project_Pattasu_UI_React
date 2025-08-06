baffa9a5490cb1f829072f148f34d0a72f878af7
Maharajan <85277330+Maharajan-02@users.noreply.github.com>
2025-08-06 16:25:59 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:8081";
-// const BASE_URL = "https://project-pattasu.onrender.com";
+//const BASE_URL = "http://localhost:8081";
+const BASE_URL = "https://project-pattasu.onrender.com";
 export default BASE_URL;
